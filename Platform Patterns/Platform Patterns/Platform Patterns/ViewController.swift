8cda15da42f0fe4482940db7b325e257c315d7f7
Samantha Gatt <smfgatt@gmail.com>
2020-02-03 10:19:04 -0500

    Adds start of Platform Patterns project

---
+++ Platform Patterns/Platform Patterns/Platform Patterns/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Platform Patterns
+//
+//  Created by Samantha Gatt on 2/3/20.
+//  Copyright © 2020 Samantha Gatt. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
